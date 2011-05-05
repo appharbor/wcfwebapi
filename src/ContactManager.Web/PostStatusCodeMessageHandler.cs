9123e6a7c94638e191e84ee80c4e91cb1655168a
Troels Thomsen <troethom@gmail.com>
2011-05-05 10:59:33 -0700

    Implement method

--- src/ContactManager.Web/PostStatusCodeMessageHandler.cs
+++ src/ContactManager.Web/PostStatusCodeMessageHandler.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ContactManager.Web
 {
@@ -7,6 +10,21 @@ namespace ContactManager.Web
 		public PostStatusCodeMessageHandler(DelegatingChannel innerChannel)
 			: base(innerChannel)
 		{
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			return SendAsync(request, cancellationToken)
+				.ContinueWith(x =>
+			{
+				var response = x.Result;
+				if (request.Method.Equals(HttpMethod.Post) && response.StatusCode == HttpStatusCode.OK)
+				{
+					response.StatusCode = HttpStatusCode.Created;
+				}
+
+				return response;
+			});
 		}
 	}
 }
