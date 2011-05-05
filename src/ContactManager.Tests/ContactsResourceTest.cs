ebb351b7e4a48a5fa2e275ba209b95d087e44628
Troels Thomsen <troethom@gmail.com>
2011-05-05 09:49:33 -0700

    Extract set up to constructor

--- src/ContactManager.Tests/ContactsResourceTest.cs
+++ src/ContactManager.Tests/ContactsResourceTest.cs
@@ -9,15 +9,21 @@ namespace ContactManager.Tests
 {
 	public class ContactsResourceTest
 	{
-		[Fact]
-		public void When_GET_then_contacts_are_returned()
+		private readonly ContactsResource _resource;
+
+		public ContactsResourceTest()
 		{
 			var repository = new Mock<IContactRepository>();
 			var fakeContacts = new List<Contact>();
 			fakeContacts.Add(new Contact { Name = "Foo Bar" });
 			repository.Setup(x => x.GetAll()).Returns(fakeContacts);
-			var contactsResource = new ContactsResource(repository.Object);
-			var contacts = contactsResource.Get().Select(x => x.Name);
+			_resource = new ContactsResource(repository.Object);
+		}
+
+		[Fact]
+		public void When_GET_then_contacts_are_returned()
+		{
+			var contacts = _resource.Get().Select(x => x.Name);
 			Assert.Equal("Foo Bar", contacts.First());
 		}
 	}
