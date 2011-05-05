8d0f9da5b3f4a8d882eb97aa685e360b7827b3c8
Troels Thomsen <troethom@gmail.com>
2011-05-05 10:52:27 -0700

    Let message handler derive from delegating channel

--- src/ContactManager.Web/PostStatusCodeMessageHandler.cs
+++ src/ContactManager.Web/PostStatusCodeMessageHandler.cs
@@ -1,6 +1,12 @@
-﻿namespace ContactManager.Web
+﻿using System.Net.Http;
+
+namespace ContactManager.Web
 {
-	public class PostStatusCodeMessageHandler
+	public class PostStatusCodeMessageHandler : DelegatingChannel
 	{
+		public PostStatusCodeMessageHandler(DelegatingChannel innerChannel)
+			: base(innerChannel)
+		{
+		}
 	}
 }
