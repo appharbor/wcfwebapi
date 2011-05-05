90dd72d2999f1387b9911cd7248ee1b5866228d3
Troels Thomsen <troethom@gmail.com>
2011-05-05 11:07:48 -0700

    Add SisoDb based repository class

---
+++ src/ContactManager.Web/Infrastructure/SisoDbContactRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Configuration;
+using SisoDb;
+
+namespace ContactManager.Web.Infrastructure
+{
+	public class SisoDbContactRepository
+	{
+		private readonly ISisoDatabase _database;
+
+		private SisoDbContactRepository()
+		{
+			string connectionString = WebConfigurationManager.ConnectionStrings["default"]
+				.ConnectionString;
+			var connectionInfo = new SisoConnectionInfo(
+				string.Format(@"sisodb:provider=Sql2008||plain:{0}", connectionString));
+			var factory = new SisoDbFactory();
+			_database = factory.CreateDatabase(connectionInfo);
+		}
+	}
+}
