f4756e473d86782a62fb593b606d2291f98c3e1e
Troels Thomsen <troethom@gmail.com>
2011-05-05 11:08:36 -0700

    Require interface implementation

--- src/ContactManager.Web/Infrastructure/SisoDbContactRepository.cs
+++ src/ContactManager.Web/Infrastructure/SisoDbContactRepository.cs
@@ -1,9 +1,11 @@
 ﻿using System.Web.Configuration;
+using System;
+using System.Collections.Generic;
 using SisoDb;
 
 namespace ContactManager.Web.Infrastructure
 {
-	public class SisoDbContactRepository
+	public class SisoDbContactRepository : IContactRepository
 	{
 		private readonly ISisoDatabase _database;
 
@@ -15,6 +17,31 @@ namespace ContactManager.Web.Infrastructure
 				string.Format(@"sisodb:provider=Sql2008||plain:{0}", connectionString));
 			var factory = new SisoDbFactory();
 			_database = factory.CreateDatabase(connectionInfo);
+		}
+
+		public void Update(Contact updatedContact)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Contact Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Contact> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Post(Contact contact)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(int id)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
